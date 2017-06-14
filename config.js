1b1997b4a5aae0104166a701cf7855f343f7bb1b
SEMIH GOKCEOGLU <semih.gkcoglu@gmail.com>
2017-06-13 20:11:03 -0400

    remove password

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ angular
     .module('process-street-app', ['ngAnimate', 'ngSanitize', 'ui.bootstrap'])
     .constant('wistiaConfig', {
         'url': 'https://upload.wistia.com',
-        'api_password': '05d33d36246ec4fb295d5e504a0e17f987a61cc57761ba1047dfc070cf1d193d'
+        'api_password': 'your password'
     });
